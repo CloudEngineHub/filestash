a794cbd80076253f5252ddefff0080542c0e6a6f
MickaelK <mickael@kerjean.me>
2026-02-16 13:05:32 +1100

    feature (plg_widget_chat): add mention

--- server/plugin/plg_widget_chat/index.go
+++ server/plugin/plg_widget_chat/index.go
@@ -18,8 +18,9 @@ var PATCH []byte
 
 func init() {
 	Hooks.Register.HttpEndpoint(func(r *mux.Router) error {
-		r.HandleFunc("/api/messages", NewMiddlewareChain(create, []Middleware{ApiHeaders, SecureHeaders, SessionStart, BodyParser})).Methods("POST")
-		r.HandleFunc("/api/messages", NewMiddlewareChain(list, []Middleware{ApiHeaders, SecureHeaders, SessionStart})).Methods("GET")
+		r.HandleFunc("/api/plg_widget_chat/messages", NewMiddlewareChain(createMessage, []Middleware{ApiHeaders, SecureHeaders, SessionStart, LoggedInOnly, BodyParser})).Methods("POST")
+		r.HandleFunc("/api/plg_widget_chat/messages", NewMiddlewareChain(listMessages, []Middleware{ApiHeaders, SecureHeaders, SessionStart, LoggedInOnly})).Methods("GET")
+		r.HandleFunc("/api/plg_widget_chat/lookup", NewMiddlewareChain(lookupUsers, []Middleware{ApiHeaders, SecureHeaders, SessionStart, LoggedInOnly})).Methods("GET")
 
 		r.HandleFunc(WithBase("/plg_handler_chat/sidebar_chat.js"), func(res http.ResponseWriter, req *http.Request) {
 			http.Redirect(res, req, WithBase("/assets/"+BUILD_REF+"/components/sidebar_chat.js"), http.StatusSeeOther)
